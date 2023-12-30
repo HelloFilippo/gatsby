7cdc7616d33c61bc42ec8cf3692b570a6da3a5bc
fil <fil@fils-Air.homenet.telecomitalia.it>
2023-12-31 00:29:06 +0100

    Good to go

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,62 @@
-import { greetUser } from '$utils/greet';
-
+const pokemonsData = [
+  {
+    id: 1,
+    name: 'Ditto',
+    sprites: {
+      other: {
+        dream_world: {
+          front_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png',
+        },
+      },
+    },
+  },
+  {
+    id: 2,
+    name: 'Bulbasaur',
+    sprites: {
+      other: {
+        dream_world: {
+          front_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png',
+        },
+      },
+    },
+  },
+  {
+    id: 3,
+    name: 'Pikachu',
+    sprites: {
+      other: {
+        dream_world: {
+          front_shiny:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png',
+        },
+      },
+    },
+  },
+];
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const itemTemplate = document.querySelector<HTMLAnchorElement>('[data-element="pokemon-item"]');
+  if (!itemTemplate) return;
+
+  const pokemonItems = pokemonsData.map(({ id, name, sprites }) => {
+    const item = itemTemplate.cloneNode(true) as HTMLAnchorElement;
+    const imageElement = item.querySelector<HTMLImageElement>('[data-element="pokemon-image"]');
+    const idElement = item.querySelector<HTMLDivElement>('[data-element="pokemon-id"]');
+    const nameElement = item.querySelector<HTMLDivElement>('[data-element="pokemon-name "]');
+
+    if (imageElement) {
+      imageElement.src = sprites.other.dream_world.front_shiny;
+    }
+    if (idElement) {
+      idElement.textContent = id.toString();
+    }
+    if (nameElement) {
+      nameElement.textContent = id.toString();
+    }
+    return item;
+  });
+  console.log(pokemonItems);
 });
